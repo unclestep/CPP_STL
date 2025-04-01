40307413c9508eb0eb42e1c05fa51e545c00618c
zarbonda <zarbonda@gmail.com>
2025-04-01 03:08:55 +0300

    almost vector

--- src/Containers/Vector/s21_vector.tpp
+++ src/Containers/Vector/s21_vector.tpp
@@ -1,7 +1,7 @@
 #include "s21_vector.h"
 template <typename T>
 Vector<T>::Vector(std::initializer_list<value_type> const &items) : m_size(items.size()), m_capacity(0U), arr(nullptr){
-    reserve_more_capacity(m_capacity);
+	reserve_more_capacity(m_size);
     std::memcpy(arr, items.begin(), items.size() * sizeof(T));
 };
 
@@ -9,9 +9,9 @@ template <typename T>
 void Vector<T>::reserve_more_capacity(size_type size) {
     if(arr == NULL){
         arr = new value_type[size];
+		m_capacity = size;
         return;
     }
-    m_capacity = m_size * 2;
     if(size > m_capacity){
         value_type *buff = new value_type[size];
         for (size_t i = 0; i < m_size; ++i) {
@@ -32,4 +32,76 @@ Vector<T>::iterator Vector<T>::begin(){
 template <typename T>
 Vector<T>::iterator Vector<T>::end(){
     return arr + m_size;
+}
+
+template <typename T>
+void Vector<T>::push_back(value_type v){
+    if (m_size == m_capacity)
+    {
+        reserve_more_capacity(m_size * 2);
+    }
+    arr[m_size++] = v;
+}
+
+template <typename T>
+Vector<T>::size_type Vector<T>::size(){
+    return m_size;
+}
+
+template <typename T>
+Vector<T>::reference Vector<T>::at(size_type pos)
+{
+    return arr[pos];
+}
+
+template <typename T>
+Vector<T>& Vector<T>::operator=(Vector<T> &&v){
+	if(this != &v){
+		arr = v.arr;
+		m_size = v.m_size;
+		m_capacity = v.m_capacity;
+
+		v.arr = nullptr;
+		v.m_size = 0;
+		v.m_capacity = 0;
+	}
+	return *this;
+}
+
+template <typename T>
+Vector<T>::reference Vector<T>::operator[](size_type pos){
+	return arr[pos];
+}
+
+template <typename T>
+Vector<T>::const_reference Vector<T>::front(){
+	return arr[0];
+}
+
+template <typename T>
+Vector<T>::const_reference Vector<T>::back(){
+	return arr[m_size-1];
+}
+
+template <typename T>
+T* Vector<T>::data(){
+	return arr;
+}
+template <typename T>
+bool Vector<T>::empty(){
+	return m_size == 0;
+}
+template <typename T>
+Vector<T>::size_type Vector<T>::max_size(){
+	return (size_t)(pow(2, 63))/(sizeof(T)) - (size_t)1;
+}
+template <typename T>
+void Vector<T>::shrink_to_fit() {
+	if(m_capacity > m_size){
+		m_size = m_capacity;
+	}
+}
+template <typename T>
+Vector<T>::size_type Vector<T>::capacity(){
+	return m_capacity;
 }
--- src/tests/s21_VectorTest.cpp
+++ src/tests/s21_VectorTest.cpp
@@ -10,5 +10,101 @@ TEST(VectorTests, Test1){
         EXPECT_EQ(counter, *it);
         counter++;
     }
+}
 
+TEST(VectorTests, Test2){
+    Vector<double> v = {1.0, 2.0, 3.0, 4.0, 5.0};
+    int counter = 1.0;
+    for(Vector<double>::iterator it = v.begin();  it != v.end(); it++){
+    EXPECT_EQ(counter, *it);
+    counter++;
+    }
+}
+
+TEST(VectorTests, Test3){
+    Vector<double> v = {1.0, 2.0, 3.0, 4.0, 5.0};
+    int counter = 1.0;
+    v.push_back(6.0);
+    for(Vector<double>::iterator it = v.begin();  it != v.end(); it++){
+        EXPECT_EQ(counter, *it);
+        counter++;
+    }
+}
+
+TEST(VectorTests, Test4){
+    Vector<double> v = {1.0, 2.0, 3.0, 4.0, 5.0};
+    v.push_back(6.0);
+    v.at(2);
+	EXPECT_EQ(v.at(2), 3.0);
+}
+
+TEST(VectorTests, Test5){
+	Vector<int> v = {1, 2, 3, 4, 5};
+	v.push_back(6);
+	Vector<int> v1;
+	v1 = std::move(v);
+	v1.at(2);
+	EXPECT_EQ(v1.at(2), 3);
+}
+
+TEST(VectorTests, Test6){
+	Vector<int> v = {1, 2, 3, 4, 5};
+	v.push_back(6);
+	EXPECT_EQ(v[0], 1);
+}
+TEST(VectorTests, Test7){
+	Vector<int> v = {1, 2, 3, 4, 5};
+	v.push_back(6);
+	v.push_back(7);
+	EXPECT_EQ(v.front(), 1);
+}
+TEST(VectorTests, Test8){
+	Vector<int> v = {1, 2, 3, 4, 5};
+	v.push_back(6);
+	EXPECT_EQ(v.back(), 6);
+}
+TEST(VectorTests, Test9){
+	Vector<int> v = {1, 2, 3, 4, 5};
+	v.push_back(6);
+	EXPECT_EQ(*v.data(), 1);
+}
+TEST(VectorTests, Test10){
+	Vector<int> v = {1, 2, 3, 4, 5};
+	v.push_back(6);
+	EXPECT_EQ(v.empty(), false);
+}
+TEST(VectorTests, Test11){
+	Vector<int> v = {};
+	EXPECT_EQ(v.empty(), true);
+}
+TEST(VectorTests, Test12){
+	Vector<int> v = {1};
+	EXPECT_EQ(v.empty(), false);
+}
+
+TEST(VectorTests, Test13){
+	Vector<int> v = {1};
+	std::vector<int> vorg;
+	EXPECT_EQ(vorg.max_size(), v.max_size());
+}
+
+TEST(VectorTests, Test14){
+	Vector<double> v = {1};
+	std::vector<double> vorg;
+	EXPECT_EQ(vorg.max_size(), v.max_size());
+}
+
+TEST(VectorTests, Test15){
+	Vector<int> v = {1};
+	EXPECT_EQ(*v.data(), v[0]);
+}
+TEST(VectorTests, Test16){
+	Vector<char> v = {'a'};
+	std::vector<char> vorg;
+	EXPECT_EQ(vorg.max_size(), v.max_size());
+}
+TEST(VectorTests, Test17){
+	Vector<int> v {5, 12, 13, 14};
+	std::vector<int> vorg {1, 2, 3, 4};
+	EXPECT_EQ(vorg.capacity(), v.capacity());
 }
